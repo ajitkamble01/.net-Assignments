93d7a909b610b8b3220cf4d843bdb3d702b18ce9
Ajit Kamble <156558966+ajitkamble01@users.noreply.github.com>
2025-03-29 11:02:22 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+//uncomment the function which you want to run, comment which you don't want to run
+
+class Program
+{
+    static void Main()
+    {
+        Student.RunStudentManagement(); // Call the function from Student.cs
+        //CalculatorApp.RunCalculator();  // Calls the function from CalculatorApp.cs
+    }
+}
